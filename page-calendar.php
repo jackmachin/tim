900dbbb1e35ac9dc46eae68b33dc8b1fd7f56124
Jack Machin <jack.machin125@gmail.com>
2016-01-12 10:26:11 +0000

    FUN

--- page-calendar.php
+++ page-calendar.php
@@ -14,7 +14,7 @@ get_header();
 if (is_user_logged_in()): ?>
 
 <section>
-    <div class="hero">
+    <div class="hero" style="background-image: url(http://www.tattoninvestments.com/tim-assets/uploads/Tree02.jpg);">
         <div class="hero__overlay"></div>
         <div class="hero__content">
             <div class="container">
@@ -29,12 +29,9 @@ if (is_user_logged_in()): ?>
 </section>
 <div class="container">
 	<div class="row mt-lg">
-		<div class="col-sm-5 mb-lg">
+		<div class="col-xs-12 content mt-lg mb-lg">
             <p>Calendar stuff
 		</div>
-        <div class="col-sm-5 col-sm-offset-2 mb-lg">
-            <p>To find out more about gaining access to our IFA resources visit our <a href="/contact">contact</a> page.</p>
-        </div>
 	</div>
 </div>
 
